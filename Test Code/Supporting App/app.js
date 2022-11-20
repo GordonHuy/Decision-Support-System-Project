b7a4ddb047bd2a5c000571b4439f1f581048d3cf
GordonHuy <ngochuy31591@gmail.com>
2022-11-20 10:21:49 +0700

    Updating

--- Test Code/Supporting App/app.js
+++ Test Code/Supporting App/app.js
@@ -1,72 +1,64 @@
-// Variable 
-
-const CartButton = document.querySelector(".cart-button")
-const CloseCartButton = document.querySelector(".close-cart")
-const ClearCartButton = document.querySelector(".clear-cart")
-const CartDOM = document.querySelector(".cart")
-const CartOverlay = document.querySelector(".cart-overlay")
-const CartItems = document.querySelector(".cart-item")
-const CartTotal = document.querySelector(".cart-total")
-const CartContent = document.querySelector(".cart-content")
-const productsDOM = document.querySelector(".ProductCenter")
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItem = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
 
 //Cart
-let Cart = [];
+let cart = [];
 
-
-//getting the product
-class Products {
-    async getProducts() {
-        try {
-            let result = await fetch("laptop_price.json")
-            let products = await result.json();
+//Getting the products
+class Products{
+    async getProducts(){
+        try{
+            let result = await fetch('laptop_price.json')
+            let products =  await result.json()
             return products;
-        } catch (error) {
-            console.log(error);
+        } catch(error){
+            console.log(error)
         }
     }
 }
 
-//display products
-class UI {
+//Display products
+class UI{
     displayProducts(products){
-        let result = "";
+        console.log(products)
+        let result = '';
         products.forEach(product => {
-             result +=`
+            result += `
             <!--Single Product-->
-            <article class="Product">
-
-                <div class="ImageContainer">
-
-                    <img src="./Image/Computer.png" alt="Product" class="ProductImage">
-
-                    <button class="BagButton", data-id=${product.laptop_ID}>
+            <article class="product">
+                <div class="img-container">
+                    <img src="./Image/Laptop.png" alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.laptop_ID}>
                         <i class="fas fa-shopping-cart"></i>
-                        Add to bag
+                        Add To Bag
                     </button>
-
                 </div>
-
                 <h3>${product.Product}</h3>
                 <h4>${product.Price_euros}</h4>
             </article>
-            <!--End of single product-->
+            <!--End of Single Product-->
             `;
         });
         productsDOM.innerHTML = result;
     }
 }
 
-
-//Local Storage
-class Storage {
+//Local storage
+class Storage{
 
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", ()=>{
     const ui = new UI();
     const products = new Products();
 
-    //get all Products
-    products.getProducts().then(products => ui.displayProducts(products))
+    //get all products
+    products.getProducts().then(products => ui.displayProducts(products));
 });
